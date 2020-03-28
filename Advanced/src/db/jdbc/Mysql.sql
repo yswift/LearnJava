1510c8323f0678f99a75392e9369db59f229d045
yswift <yanli_uoh@163.com>
2020-03-28 22:47:04 +0800

    update sql

--- Advanced/src/db/jdbc/Mysql.sql
+++ Advanced/src/db/jdbc/Mysql.sql
@@ -1,9 +1,60 @@
+create schema LearnJava collate utf8mb4_unicode_ci;
 
--- college表
 create table College
 (
-	Id nchar(2) not null,
-	Name nvarchar(50) not null,
-	constraint College_pk
-		primary key (Id)
+    Id char(2) charset utf8 not null
+        primary key,
+    Name varchar(50) charset utf8 not null
 );
+
+create table Student
+(
+    Id int auto_increment
+        primary key,
+    No varchar(50) charset utf8 null,
+    Name varchar(50) charset utf8 null,
+    Age int null,
+    Birthday date null,
+    Photo blob null
+);
+
+create table departments
+(
+    Id varchar(50) charset utf8 not null
+        primary key,
+    Name varchar(50) charset utf8 null
+);
+
+create table roles
+(
+    Id varchar(50) charset utf8 not null
+        primary key,
+    Name varchar(50) charset utf8 null
+);
+
+create table users
+(
+    Id varchar(50) charset utf8 not null
+        primary key,
+    Name varchar(50) charset utf8 null,
+    Password varchar(50) charset utf8 null,
+    Department_Id varchar(50) charset utf8 not null,
+    constraint Users_Departments_Id_fk
+        foreign key (Department_Id) references departments (Id)
+            on update cascade on delete cascade
+);
+
+create table user_role
+(
+    Id int auto_increment
+        primary key,
+    Role_Id varchar(50) charset utf8 not null,
+    User_Id varchar(50) charset utf8 not null,
+    constraint UserRole_Roles_Id_fk
+        foreign key (Role_Id) references roles (Id)
+            on update cascade on delete cascade,
+    constraint UserRole_Users_Id_fk
+        foreign key (User_Id) references users (Id)
+            on update cascade on delete cascade
+);
+
